0484caa46088d695a507551538ff10aad8821f32
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-07 18:43:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
- var deep = document.getElementById('grand-node').lastChild
+ var deep = document.getElementById('grand-node').lastElementChild
 return deep
 }
 
